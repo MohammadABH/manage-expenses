44d935e2d8c60a8afd6e41ecad2fad80fc7dd330
Mohammad <mohammad.albinhassan@kcl.ac.uk>
2020-06-20 14:54:42 +0100

    Added functionality to edit expenses to firebase instead of only redux

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -27,10 +27,18 @@ export const startAddExpense = (expenseData = {}) => {
 };
 
 // REMOVE_EXPENSE
-export const removeExpense = ({ id } = {}) => ({
+export const removeExpense = ({ id }) => ({
 	type: 'REMOVE_EXPENSE',
 	id
 });
+
+export const startRemoveExpense = ({ id } = {}) => {
+	return (dispatch) => {
+		return database.ref(`expenses/${id}`).remove().then(() => {
+			dispatch(removeExpense({ id }));
+		});
+	};
+}
 
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
@@ -39,6 +47,15 @@ export const editExpense = (id, updates) => ({
 	updates
 });
 
+export const startEditExpense = (id, updates) => {
+	return (dispatch) => {
+		return database.ref(`expenses/${id}`).update(updates).then(() => {
+			dispatch(editExpense(id, updates));
+		});
+	};
+};
+
+// SET_EXPENSES
 export const setExpenses = (expenses) => ({
 	type: 'SET_EXPENSES',
 	expenses
--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,6 +1,6 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { addExpense, editExpense, removeExpense, startAddExpense, setExpenses, startSetExpenses } from '../../actions/expenses';
+import { addExpense, editExpense, removeExpense, startAddExpense, setExpenses, startSetExpenses, startRemoveExpense, startEditExpense } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
 
@@ -22,6 +22,22 @@ test('Should setup remove expense action object', () => {
 	});
 });
 
+test('Should remove expense from firebase', (done) => {
+	const store = createMockStore({});
+	const id = expenses[0].id;
+	store.dispatch(startRemoveExpense({ id })).then(() => {
+		const actions = store.getActions();
+		expect(actions[0]).toEqual({
+			type: 'REMOVE_EXPENSE',
+			id
+		});
+		return database.ref(`expenses/${id}`).once('value');
+	}).then((snapshot) => {
+		expect(snapshot.val()).toBeFalsy();
+		done();
+	});
+});
+
 test('Should edit expense object', () => {
 	const action = editExpense(123, { note: 'New note' });
 	expect(action).toEqual({
@@ -32,6 +48,24 @@ test('Should edit expense object', () => {
 		}
 	});
 });
+
+test('Should edit expense from firebase', (done) => {
+	const store = createMockStore({});
+	const id = expenses[0].id;
+	const updates = { amount: 1023 };
+	store.dispatch(startEditExpense(id, updates)).then(() => {
+		const actions = store.getActions();
+		expect(actions[0]).toEqual({
+			type: 'EDIT_EXPENSE',
+			id,
+			updates
+		});
+		return database.ref(`expenses/${id}`).once('value');
+	}).then((snapshot) => {
+		expect(snapshot.val().amount).toBe(updates.amount);
+		done();
+	});
+});	
 
 test('Should setup expense action object with provided values', () => {
 	const expenseData = {
