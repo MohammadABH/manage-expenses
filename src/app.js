ed2478de64f60b8939fc13e7cad1014877af4ea8
Mohammad <mohammad.albinhassan@kcl.ac.uk>
2020-06-20 22:47:53 +0100

    Added private routes

--- src/app.js
+++ src/app.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'; 
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
-import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
+import { login, logout } from './actions/auth';
 import '../node_modules/normalize.css/normalize.css'
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
-import AppRouter from './routers/AppRouter';
-import './firebase/firebase';
+import AppRouter, { history } from './routers/AppRouter';
+import { firebase } from './firebase/firebase';
+
 const store = configureStore();
 
 const jsx = (
@@ -18,7 +18,29 @@ const jsx = (
 	</Provider>
 );
 
+let hasRendered = false;
+const renderApp = () => {
+	if(!hasRendered) {
+		ReactDOM.render(jsx, document.getElementById('app'));
+		hasRendered = true;
+	}
+};
+
 ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
-store.dispatch(startSetExpenses()).then(() => {
-	ReactDOM.render(jsx, document.getElementById('app'));
+
+
+firebase.auth().onAuthStateChanged((user) => {
+	if(user) {	
+		store.dispatch(login(user.uid));
+		store.dispatch(startSetExpenses()).then(() => {
+			renderApp();
+			if(history.location.pathname === '/') {
+				history.push('/dashboard');
+			}
+		});
+	} else {
+		store.dispatch(logout());
+		renderApp();
+		history.push('/')
+	}
 });
